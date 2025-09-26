cb6f44f0e53a9268e7f5f74d78249e950a7ac6d1
BeatrizMadureira <beatrizmareiralimamadureira@gmail.com>
2025-09-26 20:02:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Santos");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Peruíbe:");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Guarujá");
